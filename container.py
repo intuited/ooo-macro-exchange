d20b829b519a6ff65b6c177144aa513314383493
Ted Tibbetts <intuited@gmail.com>
2010-10-24 22:41:01 -0300

    Added name_access class, as-yet unused (and untested).

--- container.py
+++ container.py
@@ -1,4 +1,4 @@
-from collections import Sequence
+from collections import Sequence, Mapping
 class index_access(Sequence):
     """Useful for wrapping uno sequences which implement `XIndexAccess`_.
 
@@ -12,12 +12,37 @@ class index_access(Sequence):
         self._proxied = proxied
     def __len__(self):
         return self._proxied.getCount()
-    def __getitem__(self, item):
-        return self._proxied.getByIndex(item)
     def __iter__(self):
         for i in range(len(self)):
             yield self[i]
     def __contains__(self, value):
         return value in iter(self)
+    def __getitem__(self, item):
+        if item >= len(self):
+            raise IndexError("{0} outside of index range.".format(name))
+        return self._proxied.getByIndex(item)
+    def __getattr__(self, name):
+        return getattr(self._proxied, name)
+
+class name_access(Mapping):
+    """Proxies `XNameAccess`_ implementors with Pythonic dict-ness.
+
+    .. _XNameAccess:
+       http://api.openoffice.org
+             /docs/common/ref/com/sun/star/container/XNameAccess.html
+    """
+    def __init__(self, proxied):
+        self._proxied = proxied
+    def __len__(self):
+        return len(self._proxied.getElementNames())
+    def __iter__(self):
+        return iter(self._proxied.getElementNames())
+    def __contains__(self, name):
+        return self._proxied.hasByName(name)
+    def __getitem__(self, name):
+        if self._proxied.hasByName(name):
+            return self._proxied.getByName(name)
+        else:
+            raise KeyError("'{0}' not in XNameAccess object.".format(name))
     def __getattr__(self, name):
         return getattr(self._proxied, name)
